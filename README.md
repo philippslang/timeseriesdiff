97adcb24a89a3584592535d9ad79314794e56eab
plang85 <philipp.s.lang@gmail.com>
2018-04-19 20:55:38 +0100

    skeletton

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # timeseriesdiff
 Meaningful ways to compare time series.
+
+## Features
+- compares two-dimensional discrete date
+ 
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(name='timeseriesdiff',
+      version='0.1',
+      description='Meaningful comparison of discrete two-dimensional curves.',
+      author='Philipp S. Lang',
+      author_email='philipp.s.lang@gmail.com',
+      download_url='https://github.com/plang85/timeseriesdiff.git',
+      install_requires=['numpy>=1.9.1',
+                        'scipy>=0.14'],
+      extras_require={
+          'test': ['pytest',
+                   'pytest-pep8',
+                   'pytest-xdist',
+                   'pytest-cov'],
+      },
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Engineers',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=['timeseriesdiff'])
