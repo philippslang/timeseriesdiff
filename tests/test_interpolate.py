5f4ba8926164788f3482250062422aa45359bae1
plang85 <philipp.s.lang@gmail.com>
2018-04-22 21:18:38 +0100

    pytest cache to gitignore

--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -9,3 +9,12 @@ def test_area_linear():
     data = td.interpolate.preprocess(x, x)
     diff = td.interpolate.area(y0, y1, data)
     assert np.allclose([diff], [100.0])
+
+
+def test_area_adversarial():
+    x = np.linspace(0.0, 6.0 * np.pi, 100)
+    y0 = np.zeros_like(x)
+    y1 = np.sin(x)
+    data = td.interpolate.preprocess(x, x)
+    diff = td.interpolate.area(y0, y1, data)
+    assert np.allclose([diff], [0.0])
